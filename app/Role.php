1bbe2cea8e040e0fb5df21fa61dfd9ee288e8096
Bd2k9 <Bd2k9@stuff.pt>
2018-02-08 15:22:19 +0000

    admin table update

--- app/Role.php
+++ app/Role.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    
+    public function user(){
+
+        return $this->hasOne(User::class);
+    }
+
 }
