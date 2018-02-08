a6007e48cf000db07ec15241cb97d86c16bc74f3
Bd2k9 <Bd2k9@stuff.pt>
2018-02-08 15:11:13 +0000

    visual style updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,21 @@ let mix = require('laravel-mix');
  |
  */
 
+ /*
+
+    Aqui vamos encontrar todos os ficheiros css e js que importamos
+    Depois disso, usamos o npm run development para os compilar na pasta/ficheiro definido
+    na ultima linha (app.css e app.js)
+    Depois disso, via illuminate, colocamos na view
+    {!! HTML::style('css/app.css') !!}
+    {!! HTML::script('js/app.js') !!}
+
+ */
+
+
 mix.styles([
     'resources/assets/css/bootstrap.css',
     'resources/assets/css/blog-post.css',
-    'resources/assets/css/bootstrap.min.css',
     'resources/assets/css/font-awesome.css',
     'resources/assets/css/metisMenu.css',
     'resources/assets/css/sb-admin-2.css',
@@ -23,9 +34,9 @@ mix.styles([
 
 ], 'public/css/app.css').version();
 
-mix.babel([
-    'resources/assets/js/bootstrap.js',
+mix.scripts([
     'resources/assets/js/jquery.js',
+    'resources/assets/js/bootstrap.js',
     'resources/assets/js/metisMenu.js',
     'resources/assets/js/sb-admin-2.js',
     'resources/assets/js/scrips.js',
